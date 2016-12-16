4a204052f7d52431c95fa20cdae18ac79ac21735
santosh <santosh@covacsis.com>
2016-12-16 16:06:00 +0530

    REPL test runner

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -7,23 +7,30 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Created by stm on 16/12/16.
  * A very basic test to check if we are computing correctly
  * takes input from std in
- * Add a newline (press enter) and ctrl-d to end input after the newline.
+ * Ctrl -d to exit. Acts like a REPL of a calculator.
  */
 public class TestRunner {
 
     public static void main(String... args) throws IOException {
-        ANTLRInputStream input = new ANTLRInputStream(System.in);
-        CalcLexer lexer = new CalcLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        CalcParser parser = new CalcParser(tokens);
-        ParseTree tree = parser.prog();
-        ParseTreeWalker walker = new ParseTreeWalker();
-        walker.walk(new CalculatorListenerImplementation(), tree);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print (">>> ");
+            ANTLRInputStream input = new ANTLRInputStream(scanner.nextLine()+ "\n");
+            CalcLexer lexer = new CalcLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            CalcParser parser = new CalcParser(tokens);
+            ParseTree tree = parser.prog();
+            ParseTreeWalker walker = new ParseTreeWalker();
+            walker.walk(new CalculatorListenerImplementation(), tree);
+            System.out.println();
+        }
+
     }
 
 }
